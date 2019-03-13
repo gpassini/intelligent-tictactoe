ada5142a3fed72df74da0ab61d7e4f967d71b3d9
g.passini <g.passini@groupeonepoint.com>
2019-03-12 20:46:51 -0400

    Beginning of minimax implementation

---
+++ src/main/kotlin/com/intelligenttictactoe/demo/TicTacToeMinimaxPlayer.kt
@@ -0,0 +1,10 @@
+package com.intelligenttictactoe.demo
+
+class TicTacToeMinimaxPlayer(private val symbol: TicTacToeSquare) {
+    fun play(board: TicTacToeBoard) {
+    }
+
+    private fun minimax(board: TicTacToeBoard, max: Boolean) {
+
+    }
+}
